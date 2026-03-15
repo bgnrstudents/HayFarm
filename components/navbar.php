fbb2df804596f11052ae46e142b103267d7db991
Muhammad Rizki <pelajar668@gmail.com>
2026-03-16 00:07:18 +0700

    components navbar

---
+++ components/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg px-5">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+        <!-- LOGO -->
+        <a class="navbar-brand" href="#">
+            <img src="public/images/logo/logo1.png" style="height:50px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- MENU -->
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tentang Kami</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Produk</a>
+                </li>
+            </ul>
+            <div class="d-lg-flex d-block gap-2">
+                <button class="btn ">Login</button>
+                <button class="btn btn-success">Register</button>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ pages/user/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- TITLE -->
+    <title>HayFarm</title>
+    <!-- BOOTSTRAP CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <?php include 'components/navbar.php'; ?>
+
+
+    <!-- CONTENT -->
+    <div class="container mt-4">
+        
+       <h1 class="h1">HALAMAN UTAMA'</h1>
+       <h1 class="h11">HALAMAN UTAMA 2'</h1>
+
+
+    </div>
+
+
+    <!-- FOOTER -->
+    <?php include 'components/footer.php'; ?>
+
+
+    <!-- BOOTSTRAP JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> 
+    <!-- MAIN JS -->
+    <script src="public/js/script.js"></script>
+
+</body>
+</html>
+
