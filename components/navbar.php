b76a62bc083c528c50d6a41461bddb33cf217e46
Muhammad Rizki <pelajar668@gmail.com>
2026-03-16 07:57:32 +0700

    update navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,29 +1,56 @@
-<nav class="navbar navbar-expand-lg px-5">
-    <div class="container-fluid d-flex justify-content-between align-items-center">
-        <!-- LOGO -->
-        <a class="navbar-brand" href="#">
-            <img src="public/images/logo/logo1.png" style="height:50px;">
+<nav class="navbar navbar-expand-lg bg-white shadow-sm px-5">
+
+  <div class="container-fluid">
+
+    <!-- LOGO -->
+    <a class="navbar-brand" href="#">
+      <img src="public/images/logo/logo1.png" style="height:50px;">
+    </a>
+
+    <!-- HAMBURGER MOBILE -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- NAVBAR CONTENT -->
+    <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+
+      <!-- MENU -->
+      <ul class="navbar-nav mx-auto">
+
+        <li class="nav-item">
+          <a class="nav-link active" href="/index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/pages/user/tentang_kami.php">Tentang Kami</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/pages/user/produk.php">Produk</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/pages/user/keranjang.php">Keranjang</a>
+        </li>
+
+      </ul>
+
+      <!-- BUTTON -->
+      <div class="d-flex gap-2">
+
+        <a href="/login.php" class="btn btn-outline-success">
+          Login
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- MENU -->
-        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Tentang Kami</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Produk</a>
-                </li>
-            </ul>
-            <div class="d-lg-flex d-block gap-2">
-                <button class="btn ">Login</button>
-                <button class="btn btn-success">Register</button>
-            </div>
-        </div>
+
+        <a href="/register.php" class="btn btn-success">
+          Register
+        </a>
+
+      </div>
+
     </div>
+
+  </div>
+
 </nav>
