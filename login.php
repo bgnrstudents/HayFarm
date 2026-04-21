40cdf56e1b4849bfd930c2b647085d30925ddaff
Muhammad Rizki <pelajar668@gmail.com>
2026-04-21 14:39:14 +0700

    commit coba

--- login.php
+++ login.php
@@ -39,145 +39,17 @@ if( isset($_POST['submit']) ){
     } 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<!-- <html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport"$dbtent="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!-- nunito -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet"></style>
-<!-- costum css -->
-<style>
-body {
-    margin: 0;
-    padding: 0;
-    background: url('images/bg_login.png') no-repeat center center fixed;
-    background-size: cover;
-}
-section{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100%;
-    color: #fff;
-}
-.form-box{
-    position: relative;
-    width: 630px;
-    height: 450px;
-    background-color: rgb(255, 255, 255);
-    border: 2px solid rgba(0, 0, 0, 0.42);
-    border-radius: 20px;
-    backdrop-filter: blur(15px);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-h4{
-    font-family: 'Nunito', sans-serif;
-    font-size: 1.5rem;
-    color: black;
-    text-align: center;
-    margin-bottom: 2%;
-    margin-top: 5%;
-    text-shadow: 9%;
-}
-p {
-    font-family: 'Nunito', sans-serif;
-    font-size: 0.9rem;
-    color: black;
-    text-align: center;
-    margin-bottom: 8%;
-    text-shadow: 9%;
-}
-.form-container {
-    background: rgb(255, 255, 255);
-    padding: 20px;
-    border-radius: 10px;
-    text-align: center;
-    width: 1030px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
-    margin: 0 auto;
-}
-.form-box{
-    position: relative;
-    margin: 20px 0;
-    width: 630px;
-    border-bottom: 2px solid #fff;
-}
-.form-box label{
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    transition: all .5s ease-in-out;
-    color: #fafafa;
-    font-size: 1rem;
-    pointer-events: none;
-}
-.form-control{
-    position: relative;
-    /* margin: 5px; */
-    width: 500px;
-    height: 46px;
-    background: rgba(241, 249, 244, 1);
-    border-radius: 8px;
-    font-size: 15px;
-    font-family: 'Nunito', sans-serif;
-    font-weight: semibold;
-}
-.form-group label{
-    color: #000000;
-    font-weight: semibold;
-    font-family: 'Nunito', sans-serif;
-    justify-content: left;
-    align-items: start;
-}
-input:focus~label, input:valid~label{
-    top: -5px;
-}
-.inputbox input{
-    width: 350px;
-    height: 60px;
-    background: transparent;
-    border: none;
-    outline: none;
-    font-size: 1rem;
-    padding: 0 35px 0 5px;
-    color: #fff;
-}
-button{
-    width: 318px;
-    height: 46px;
-    border-radius: 8px;
-    background: rgba(25, 108, 51, 1);
-    border: none;
-    outline: none;
-    cursor: pointer;
-    font-size: 20px;
-    color: #fff;
-}
-.form-footer{
-    font-size: .9rem;
-    color: #009b48;
-    text-align: center;
-    margin: 25px 0 10px;
-}
-.form-footer p a{
-    text-decoration: none;
-    color: #009b48;
-    font-weight: 600;
-}
-.form-footer p a:hover{
-    text-decoration: underline;
-}
-</style>
 </head>
-<body>
+<body> -->
         <section class="container-fluid mb-4">
             <section class="row justify-content-center">
             <section class="col-12 col-sm-6 col-md-4">
@@ -225,10 +97,10 @@ button{
             </section>
             </section>
         </section>
-        <script src="script.js"></script>
+        <!-- <script src="script.js"></script> -->
     <!-- Bootstrap requirement jQuery pada posisi pertama, kemudian Popper.js, dan  yang terakhit Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
-</html>
+</html> -->
